aa5f64f0a068283e19e3b6f1f0c18091d6ac7b60
Bala Krishna Ragala <baluragala@gmail.com>
2018-08-23 20:03:47 +0530

    redirect & render

--- estore/src/App.js
+++ estore/src/App.js
@@ -4,27 +4,50 @@ import "./App.css";
 import Header from "./components/Header";
 import ProductList from "./components/ProductList";
 import AddProduct from "./components/AddProduct";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ProductDetail from "./components/ProductDetail";
 import NotFound from "./components/NotFound";
+import Login from "./components/Login";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isAdmin: false };
+  }
+
   getTitle() {
     return "Welcome to React - from method";
   }
 
   render() {
-    const message = "Welcome to React";
     return (
       <div className="App">
         <Header siteTitle="E-STORE" subTitle="this is sub" />
         <NavBar />
+        <button onClick={() => this.setState({ isAdmin: true })}>
+          Make Admin
+        </button>
         {/* this is comment */}
         <Switch>
-          <Route exact={true} path="/products" component={ProductList} />
-          <Route path="/products/new" component={AddProduct} />
+          <Route
+            exact={true}
+            path="/products"
+            render={props => <ProductList {...props} additionalProp="test" />}
+          />
+          <Route
+            path="/products/new"
+            render={props => {
+              return this.state.isAdmin ? (
+                <AddProduct />
+              ) : (
+                <Redirect to="/login" />
+              );
+            }}
+          />
           <Route exact path="/products/:productId" component={ProductDetail} />
+          <Route exact path="/login" component={Login} />
+
           <Route component={NotFound} />
         </Switch>
       </div>
--- estore/src/components/NotFound.js
+++ estore/src/components/NotFound.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 
-class NotFound extends Component {
-  render() {
-    const { history } = this.props;
-    return (
-      <div>
-        <h2>Oops.. the page you are looking is not found</h2>
-        <button onClick={() => history.goBack()}>Go Back</button>
-        <button onClick={() => history.push("/products")}>
-          Go To Products
-        </button>
-      </div>
-    );
-  }
+// class NotFound extends Component {
+//   render() {
+//     const { history } = this.props;
+//     return (
+//       <div>
+//         <h2>Oops.. the page you are looking is not found</h2>
+//         <button onClick={() => history.goBack()}>Go Back</button>
+//         <button onClick={() => history.push("/products")}>
+//           Go To Products
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+function NotFound({ location, match, history }) {
+  return (
+    <div>
+      <h2>Oops.. the page you are looking is not found</h2>
+      <button onClick={() => history.goBack()}>Go Back</button>
+      <button onClick={() => history.push("/products")}>Go To Products</button>
+    </div>
+  );
 }
 
 export default NotFound;
