80c9a76d60741bb1db816e2f5b7f68d16a62d404
Bala Krishna Ragala <baluragala@gmail.com>
2018-08-23 19:30:08 +0530

    history

--- estore/src/components/NotFound.js
+++ estore/src/components/NotFound.js
@@ -2,9 +2,14 @@ import React, { Component } from "react";
 
 class NotFound extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <h2>Oops.. the page you are looking is not found</h2>
+        <button onClick={() => history.goBack()}>Go Back</button>
+        <button onClick={() => history.push("/products")}>
+          Go To Products
+        </button>
       </div>
     );
   }
