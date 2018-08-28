c8b4a0da2acb58e5ce78187e71f8fa0545709fae
Bala Krishna Ragala <baluragala@gmail.com>
2018-08-28 19:27:20 +0530

    intial commit

---
+++ react-scratch/src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React 16!!!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"));
